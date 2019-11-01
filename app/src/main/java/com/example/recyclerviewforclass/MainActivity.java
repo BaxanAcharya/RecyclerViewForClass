d39434c3888595d4258f4189a029702010250c92
BaxanAcharya <baxanacharya@gmail.com>
2019-11-01 14:41:35 +0545

    recycleview in linear layout

--- app/src/main/java/com/example/recyclerviewforclass/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewforclass/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.recyclerviewforclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.recyclerviewforclass.adapter.ContactsAdapter;
+import com.example.recyclerviewforclass.model.Contacts;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+        //making list
+        List<Contacts> contactsList=new ArrayList<>();
+        contactsList.add(new Contacts("Bhuwan", "9845895452", R.drawable.bhuwan));
+        contactsList.add(new Contacts("Dahayang", "9845203157", R.drawable.dahayang));
+        contactsList.add(new Contacts("Saugat", "9812457820", R.drawable.saugat));
+        contactsList.add(new Contacts("Bhuwan", "9845895452", R.drawable.bhuwan));
+        contactsList.add(new Contacts("Dahayang", "9845203157", R.drawable.dahayang));
+        contactsList.add(new Contacts("Saugat", "9812457820", R.drawable.saugat));
+        ContactsAdapter contactsAdapter=new ContactsAdapter(this, contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
--- app/src/main/java/com/example/recyclerviewforclass/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/recyclerviewforclass/adapter/ContactsAdapter.java
@@ -1,18 +1,59 @@
 package com.example.recyclerviewforclass.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerviewforclass.R;
+import com.example.recyclerviewforclass.model.Contacts;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ContactsAdapter {
+public class ContactsAdapter  extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>{
 
-public class ContactsViewHolder extends RecyclerView.ViewHolder{
+    Context mContext;
+    List<Contacts> contactsList;
+
+    public ContactsAdapter(Context mContext, List<Contacts> contactsList) {
+        this.mContext = mContext;
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //view holder ma attach garni
+
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_contacts,parent, false);
+        return new ContactsViewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+    //data lai set garni
+        Contacts contacts=contactsList.get(position);
+        holder.imgProfile.setImageResource(contacts.getImageId());
+        holder.textViewphone.setText(contacts.getPhoneNo());
+        holder.textViewname.setText(contacts.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        //kati ota data dekhauni
+        return contactsList.size();
+    }
+
+
+    public class ContactsViewHolder extends RecyclerView.ViewHolder{
 
     CircleImageView imgProfile;
     TextView textViewname;
